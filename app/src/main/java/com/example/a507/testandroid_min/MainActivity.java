221ecf833f81baf5933794671f5a869248478a11
minjohney <dtropy0217@naver.com>
2018-10-02 16:13:36 +0900

    homepage make

--- app/src/main/java/com/example/a507/testandroid_min/MainActivity.java
+++ app/src/main/java/com/example/a507/testandroid_min/MainActivity.java
@@ -1,14 +1,27 @@
 package com.example.a507.testandroid_min;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    protected Button btHomePage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btHomePage = (Button) findViewById(R.id.btHomepage);
+        btHomePage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://ice.mokwon.ac.kr"));
+                startActivity(intent);
+            }
+        });
 
     }
 }
