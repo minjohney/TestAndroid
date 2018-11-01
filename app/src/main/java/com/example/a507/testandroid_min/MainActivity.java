2ab90afe09b071c685ec10a8781b03dce8a06bf7
minjohney <dtropy0217@naver.com>
2018-11-01 12:51:20 +0900

    echo App

--- app/src/main/java/com/example/a507/testandroid_min/MainActivity.java
+++ app/src/main/java/com/example/a507/testandroid_min/MainActivity.java
@@ -18,99 +18,94 @@ import java.util.ArrayList;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity implements TextToSpeech.OnInitListener {
-    protected Button btHomePage, btDial, btCall, btSMS, btMAP, btREC, btTTS;
+    protected Button  btREC, btTTS, btEcho;
     protected TextView txRcog;
-    protected EditText extts;
+    protected EditText extts,etRelay;
     protected TextToSpeech tts;
-    private static int CODE_RECOG = 1234;
+    private static int CODE_RECOG = 1234, CODE_ECHO = 1227;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btHomePage = (Button) findViewById(R.id.btHomepage);
-        btHomePage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://ice.mokwon.ac.kr"));
-                startActivity(intent);
-            }
-        });
+
         extts = (EditText) findViewById(R.id.extts);
         btTTS = (Button) findViewById(R.id.btTTS);
         btTTS.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String str = extts.getText().toString();
-                tts.speak(str, TextToSpeech.QUEUE_FLUSH, null, null);
+               speakStr(extts.getText().toString());
             }
         });
-        tts = new TextToSpeech(this,this)
+        tts = new TextToSpeech(this,this);
+        btEcho = (Button) findViewById(R.id.btEcho);
+        btEcho.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                voiceRecog(CODE_ECHO);
+            }
+        });
+        etRelay = (EditText) findViewById(R.id.etRelay);
 
-
-        btDial = (Button) findViewById(R.id.btDial);
-        btDial.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:0428297670"));
-                startActivity(intent);
-            }
-        });
-        btCall = (Button) findViewById(R.id.btCall);
-        btCall.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:0428297670"));
-                startActivity(intent);
-            }
-        });
-        btSMS = (Button) findViewById(R.id.btSMS);
-        btSMS.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Intent.ACTION_SENDTO, Uri.parse("smsto:0428297670"));
-                intent.putExtra("sms_body", "Mokwon University");
-                startActivity(intent);
-            }
-        });
-        btMAP = (Button) findViewById(R.id.btMAP);
-        btMAP.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("geo:36.321609,127.337957?z=20"));
-                startActivity(intent);
-            }
-        });
         txRcog = (TextView) findViewById(R.id.txRecog);
         btREC = (Button) findViewById(R.id.btREC);
         btREC.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                voiceRecog();
+                voiceRecog(CODE_RECOG);
 
             }
         });
 
+
+
     }
 
-    private void voiceRecog() {
+    private  void speakStr(String str){
+        tts.speak(str,TextToSpeech.QUEUE_FLUSH, null,null);
+        while(tts.isSpeaking()){
+            try{
+                Thread.sleep(100);
+            } catch(InterruptedException e)
+            {
+                e.printStackTrace();
+            }
+        }
+    }
+
+        private void voiceRecog(int nCode){
         Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
         //FREE_FORM : 내가 가지고 있는 정보 확인, WEB : 서버 정보에 있는 것까지
         intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Please Speak..");
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.KOREAN);
-        startActivityForResult(intent, CODE_RECOG);
+        startActivityForResult(intent, nCode);
     }
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data); //부모함수 호출
-        if (requestCode == CODE_RECOG) {
-            if (resultCode == Activity.RESULT_OK && data != null) {
-                ArrayList<String> arList = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+        if (resultCode == Activity.RESULT_OK && data != null)
+        {
+            if (requestCode == CODE_RECOG) {
+            ArrayList<String> arList = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
                 String sRecog = arList.get(0);
                 txRcog.setText(sRecog);
 
+            }
+            else if(requestCode==CODE_ECHO){
+                ArrayList<String> arList = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                String sRecog = arList.get(0);
+                String sDelay = etRelay.getText().toString();
+                int nDelay = Integer.parseInt(sDelay); //단위는 초단위
+                try {
+                    Thread.sleep(nDelay*1000); //단위는 밀리초단위
+
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                speakStr(sRecog);
             }
 
         }
@@ -120,8 +115,8 @@ public class MainActivity extends AppCompatActivity implements TextToSpeech.OnIn
     public void onInit(int status) {
         if (status == TextToSpeech.SUCCESS) {
             tts.setLanguage(Locale.KOREAN);
-            tts.setPitch(0.6f);
-            tts.setSpeechRate(1f);
+            tts.setPitch(1.0f);
+            tts.setSpeechRate(1.0f);
         }
     }
 }
