6b200703183879016f7e849b542ffe0b39f28f82
minjohney <dtropy0217@naver.com>
2018-11-06 16:12:27 +0900

    [2018-11-06] contacts

--- app/src/main/java/com/example/a507/testandroid_min/MainActivity.java
+++ app/src/main/java/com/example/a507/testandroid_min/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.a507.testandroid_min;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.net.Uri;
+import android.provider.ContactsContract;
 import android.speech.RecognizerIntent;
 import android.speech.tts.TextToSpeech;
 import android.support.annotation.Nullable;
@@ -13,16 +15,18 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Currency;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity implements TextToSpeech.OnInitListener {
-    protected Button  btREC, btTTS, btEcho;
+    protected Button btREC, btTTS, btEcho, btCall, btDial, btContact, btMap, btHome, btSms;
     protected TextView txRcog;
-    protected EditText extts,etRelay;
+    protected EditText extts, etRelay;
     protected TextToSpeech tts;
-    private static int CODE_RECOG = 1234, CODE_ECHO = 1227;
+    private static int CODE_RECOG = 1234, CODE_ECHO = 1227, CODE_CONTACT = 1529; //숫자가 큰 경우 에러, 최대 4자리로
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,10 +38,10 @@ public class MainActivity extends AppCompatActivity implements TextToSpeech.OnIn
         btTTS.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               speakStr(extts.getText().toString());
+                speakStr(extts.getText().toString());
             }
         });
-        tts = new TextToSpeech(this,this);
+        tts = new TextToSpeech(this, this);
         btEcho = (Button) findViewById(R.id.btEcho);
         btEcho.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -57,23 +61,80 @@ public class MainActivity extends AppCompatActivity implements TextToSpeech.OnIn
             }
         });
 
+        btContact = (Button) findViewById(R.id.btContact);
+        btContact.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK); //사용자의 선택에 따라 (사용자의 permission 이 된 상태)
+                intent.setData(ContactsContract.CommonDataKinds.Phone.CONTENT_URI); //Uri 또다른 입력 방법
+                startActivityForResult(intent, CODE_CONTACT); //사용자 정보를 받기 위함(코드가 맞는 경우에만 접근이 가능하도록 암호를 지정)
+            }
+        });
+
+        btDial = (Button) findViewById(R.id.btDial);
+        btDial.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:0428297670"));
+                startActivity(intent);
+
+            }
+        });
+        btCall = (Button) findViewById(R.id.btCall);
+        btCall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:0428297670"));
+                startActivity(intent);
+
+            }
+        });
+        btMap = (Button) findViewById(R.id.btMap);
+        btMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("geo:36.321609,127.337957?z=20"));
+                startActivity(intent);
+
+
+            }
+        });
+        btHome = (Button) findViewById(R.id.btHome);
+        btHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://ice.mokwon.ac.kr"));
+                startActivity(intent);
+
+            }
+        });
+        btSms = (Button) findViewById(R.id.btSms);
+        btSms.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_SENDTO, Uri.parse("smsto:0428297670"));
+                intent.putExtra("sms_body", "Mokwon University");
+                startActivity(intent);
+
+
+            }
+        });
 
 
     }
 
-    private  void speakStr(String str){
-        tts.speak(str,TextToSpeech.QUEUE_FLUSH, null,null);
-        while(tts.isSpeaking()){
-            try{
+    private void speakStr(String str) {
+        tts.speak(str, TextToSpeech.QUEUE_FLUSH, null, null);
+        while (tts.isSpeaking()) {
+            try {
                 Thread.sleep(100);
-            } catch(InterruptedException e)
-            {
+            } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
     }
 
-        private void voiceRecog(int nCode){
+    private void voiceRecog(int nCode) {
         Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
         //FREE_FORM : 내가 가지고 있는 정보 확인, WEB : 서버 정보에 있는 것까지
@@ -86,26 +147,38 @@ public class MainActivity extends AppCompatActivity implements TextToSpeech.OnIn
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data); //부모함수 호출
-        if (resultCode == Activity.RESULT_OK && data != null)
-        {
+        if (resultCode == Activity.RESULT_OK && data != null) {
             if (requestCode == CODE_RECOG) {
-            ArrayList<String> arList = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                ArrayList<String> arList = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
                 String sRecog = arList.get(0);
                 txRcog.setText(sRecog);
 
-            }
-            else if(requestCode==CODE_ECHO){
+            } else if (requestCode == CODE_ECHO) {
                 ArrayList<String> arList = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
                 String sRecog = arList.get(0);
                 String sDelay = etRelay.getText().toString();
                 int nDelay = Integer.parseInt(sDelay); //단위는 초단위
                 try {
-                    Thread.sleep(nDelay*1000); //단위는 밀리초단위
+                    Thread.sleep(nDelay * 1000); //단위는 밀리초단위
 
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
                 speakStr(sRecog);
+            } else if (requestCode == CODE_CONTACT) {
+                String[] sFilter = {ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME, ContactsContract.CommonDataKinds.Phone.NUMBER};
+                Cursor cursor =  getContentResolver().query(data.getData(), sFilter, null, null, null);
+                //ContentResolver 클래스를 불러옴/다른 manager와 달리 resolver라 붙임, 앞에서 setData  한것을 getData 하여 Uri를 불러옴
+                //Cursor: 결과를 추적하기 위한 지칭
+                if(cursor != null)
+                {
+                    cursor.moveToFirst();
+                    String sName = cursor.getString(0);
+                    String sPhoneNum = cursor.getString(1);
+                    cursor.close();
+                    Toast.makeText(this, sName + "=" + sPhoneNum, Toast.LENGTH_LONG).show();
+                }
+
             }
 
         }
---
+++ mypush.bat
@@ -0,0 +1,3 @@
+git add --all
+git commit -m "[2018-11-06] contacts"
+git push -u origin master
